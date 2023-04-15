fa61d3a0a030616f0dee585572c917a945fcab84
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-15 14:56:52 +0500

    Initial commit

--- CustomButtonSheetSwiftUI/ContentView.swift
+++ CustomButtonSheetSwiftUI/ContentView.swift
@@ -7,20 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ContentView <Content: View>: View {
+    
+    let content: () -> Content
+    
+    @GestureState private var gestureOffset = CGSize.zero
+    @State private var currentMenuOffsetY: CGFloat = 0.0
+    @State private var lastMenuOffsetY: CGFloat = 0.0
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .updating($gestureOffset) { value, state, transition in
+                state = value.translation
+                onChangeMenuOffset()
+            }
+            .onEnded { value in
+                
+                let maxHeight = UIScreen.main.bounds.height - 100
+                
+                
+                withAnimation {
+                    if -currentMenuOffsetY > maxHeight {
+                        currentMenuOffsetY = -maxHeight
+                    } else {
+                        currentMenuOffsetY = 0 
+                    }
+                    lastMenuOffsetY = currentMenuOffsetY
+                }
+            }
+    }
+    
+    func onChangeMenuOffset() {
+        DispatchQueue.main.async {
+            self.currentMenuOffsetY = gestureOffset.height + lastMenuOffsetY
         }
-        .padding()
+        
+    }
+    
+    var body: some View {
+        
+        ZStack{
+            Color("background")
+            
+            content()
+            
+            .frame(height: UIScreen.main.bounds.height + 100)
+            .background(RoundedRectangle(cornerRadius: 20).fill(Color("alert")))
+            .ignoresSafeArea(.all, edges: .bottom)
+            .offset(y: UIScreen.main.bounds.height)
+            .offset(y: currentMenuOffsetY)
+            .gesture(dragGesture)
+        }
+       
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView {
+            VStack {
+                
+                Capsule()
+                    .fill(.white)
+                    .frame(width: 80, height: 3)
+                    .padding(.top)
+                
+                ForEach(0..<4) {_ in
+                    RoundedRectangle(cornerRadius: 20 )
+                        .fill(Color("action"))
+                        .frame(height: 100)
+                        .padding(.horizontal)
+                }
+                Spacer()
+            }
+        }
     }
 }
--- CustomButtonSheetSwiftUI/CustomButtonSheetSwiftUIApp.swift
+++ CustomButtonSheetSwiftUI/CustomButtonSheetSwiftUIApp.swift
@@ -11,7 +11,23 @@ import SwiftUI
 struct CustomButtonSheetSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView {
+                VStack {
+                    
+                    Capsule()
+                        .fill(.white)
+                        .frame(width: 80, height: 3)
+                        .padding(.top)
+                    
+                    ForEach(0..<1) {_ in
+                        RoundedRectangle(cornerRadius: 20 )
+                            .fill(Color("action"))
+                            .frame(height: 400)
+                            .padding(.horizontal)
+                    }
+                    Spacer()
+                }
+            }
         }
     }
 }
